d598868208dd021c5eb7a59bfcb231fc3c8d4920
Jar'Zeno <jarzenojarrett@gmail.com>
2023-02-07 17:01:46 -0800

    Add join and leave btns

--- src/App.js
+++ src/App.js
@@ -8,25 +8,47 @@ const App = () => {
   const rtcProps = {
     appId: '499da72755484274a1f430c5a9a9ce17',
     channel: 'test',
-    token: '007eJxTYGBumbHp/XL76MU1BRNkFqxMPNH2xnKOR+MW/6oDHdYvA0UVGEwsLVMSzY3MTU1NLEyMzE0SDdNMjA2STRMtEy2TUw3N/6jfTW4IZGS47c7OwAiFID4LQ0lqcQkDAwBdcx+D',
+    token: '007eJxTYAhTMNm9QXar2YHvtbrM+470Gf19e0fFZ2bQja3cW+O7D9UqMJhYWqYkmhuZm5qaWJgYmZskGqaZGBskmyZaJlompxqa7/n0KLkhkJHh+4UAJkYGCATxWRhKUotLGBgAi4IhUA==',
   };
+
   const callbacks = {
     EndCall: () => setInCall(false),
   };
 
+  const createNewCall = () => {
+
+  };
+
   return (
     <div>
+      <div
+        className='flex gap-1'
+      >
+        <button 
+        className='p-2 bg-neutral-400 text-lg'
+        onClick={() => setInCall(true)}
+        >
+          Start Call
+        </button>
+        <button
+          className='p-2 bg-neutral-400 text-lg'
+          onClick={() => setInCall(false)}
+        >
+          Leave Call
+        </button>
+        <button
+          className='p-2 bg-neutral-400 text-lg'
+        >
+          Join Call
+        </button>
+      </div>
+      
       { 
         inCall ? 
           <div style={{display: 'flex', width: '100vw', height: '100vh'}}>
             <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
           </div> :
-          <button 
-            className='p-2 bg-neutral-400 text-lg'
-            onClick={() => setInCall(true)}
-          >
-            Start Call
-          </button>
+          null
       }
     </div>
   );
