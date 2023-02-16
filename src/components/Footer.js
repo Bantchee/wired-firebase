e6e7ecfe855b70de9b1fee9dd92526759179b0e3
Jar'Zeno <jarzenojarrett@gmail.com>
2023-02-16 08:17:20 -0800

    Update github link style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,9 @@ const Footer = () => {
             >
                 Made By J. Steven Jarrett
             </p>
-            <a href="https://github.com/Bantchee" target={"_blank"} rel="noreferrer">
+            <a href="https://github.com/Bantchee" target={"_blank"} rel="noreferrer"
+                className="hover:bg-neutral-200 rounded-full"
+            >
                 <FontAwesomeIcon icon={faGithub} size="4x"/>
             </a>
         </div>
