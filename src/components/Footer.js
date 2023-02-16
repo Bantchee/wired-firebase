baa2001f2f3e34dfe5c2041fc098469d85fa7a4c
Jar'Zeno <jarzenojarrett@gmail.com>
2023-02-16 08:15:36 -0800

    Add styling to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Footer = () => {
     return (
-        <div>
-            <p>Made By J. Steven Jarrett</p>
+        <div
+            className="flex gap-4 justify-center items-center"
+        >
+            <p
+                className="text-4xl font-bold"
+            >
+                Made By J. Steven Jarrett
+            </p>
             <a href="https://github.com/Bantchee" target={"_blank"} rel="noreferrer">
-                <FontAwesomeIcon icon={faGithub} />
+                <FontAwesomeIcon icon={faGithub} size="4x"/>
             </a>
         </div>
     )
